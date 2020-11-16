7a710357b6cf0ac797c76b77dcad966d01d16bce
MasudaAfrin <masudatuba@gmail.com>
2020-11-16 13:38:37 +0600

    line controller create action update

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,6 +30,7 @@ class LineItemsController < ApplicationController
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product)
 
+    render :new if @line_item == nil?
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_index_url, notice: 'Line item was successfully created.' }
